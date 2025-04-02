ebd30b85463878970a6e234fa88f73fe5e9fba34
Raphael <604332093@qq.com>
2025-04-02 19:31:37 +0800

    feat: update

--- WebFrontend/config/proxy.ts
+++ WebFrontend/config/proxy.ts
@@ -1,15 +1,7 @@
 export default {
   dev: {
     '/api': {
-      target: 'http://47.119.148.125:9900',
-      // target: 'http://47.116.215.74:88',
-      changeOrigin: true,
-      pathRewrite: { '^': '' },
-    }
-  },
-  test: {
-    '/api/': {
-      target: 'http://47.116.215.74:88',
+      target: 'http://localhost:8080',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     }
