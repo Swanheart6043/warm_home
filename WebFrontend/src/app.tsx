933d61dac80b64047027b7d89db096755b2a3667
Raphael <604332093@qq.com>
2025-05-23 16:06:34 +0800

    feat: 新增一个doc文件夹，将图纸文件放入其中

--- WebFrontend/src/app.tsx
+++ WebFrontend/src/app.tsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import type { MenuProps } from 'antd';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const items: Required<MenuProps>['items'][number][] = [
+    {
+      key: 'sub1',
+      label: '首页',
+      icon: <MailOutlined />,
+    },
+    {
+      key: 'sub1',
+      label: '硬件控制',
+      icon: <MailOutlined />,
+      children: [
+        { key: 'g1', label: '灯具', type: 'group' },
+        { key: 'g2', label: '音箱', type: 'group' },
+      ],
+    },
+    {
+      key: 'sub2',
+      label: '环境信息',
+      icon: <AppstoreOutlined />,
+      children: [
+        { key: '5', label: '温度' },
+        { key: '6', label: '湿度' },
+        { key: 'sub3', label: 'Submenu' },
+      ],
+    },
+    {
+      key: 'sub3',
+      label: '实时监控',
+      icon: <AppstoreOutlined />,
+      children: [
+        { key: '5', label: 'Option 5' },
+        { key: '6', label: 'Option 6' },
+      ],
+    },
+    {
+      key: 'sub3',
+      label: '历时照片',
+      icon: <AppstoreOutlined />,
+      children: [
+        { key: '5', label: 'Option 5' },
+        { key: '6', label: 'Option 6' },
+      ],
+    },
+  ];
 
   return (
-    <>
+    <div className='w-full h-full flex'>
+      <div className='flex-[0_0_256]'>
+        <Menu
+          theme="dark"
+          style={{ width: 256 }}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          items={items}
+        />
+      </div>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div></div>
+
+        <div></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
