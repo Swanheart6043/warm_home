7ebd6dcdf622687a194c315b9806a911e1ba88f6
Raphael <604332093@qq.com>
2025-05-26 15:20:33 +0800

    feat: 更新前端页面

---
+++ WebFrontend/src/Dashboard/Environmental.tsx
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { useEffect } from "react"
+
+export const Environmental = () => {
+  useEffect(() => {
+    axios.get("")
+  }, [])
+
+  return (
+    <div></div>
+  )
+}
--- WebFrontend/src/app.tsx
+++ WebFrontend/src/app.tsx
@@ -1,49 +1,45 @@
 import './App.css'
-import { FileImageOutlined, CrownOutlined, WindowsOutlined, DotChartOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
+import { FileImageOutlined, CrownOutlined, WindowsOutlined, DotChartOutlined, CameraOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Menu } from 'antd';
 import type { MenuProps } from 'antd';
 
 function App() {
   const items: Required<MenuProps>['items'][number][] = [
     {
-      key: 'sub1',
+      key: 'home',
       label: '首页',
       icon: <CrownOutlined />,
     },
     {
-      key: 'sub2',
+      key: 'environmental',
       label: '环境信息',
       icon: <DotChartOutlined />,
-      children: [
-        { key: '1', label: '温度' },
-        { key: '2', label: '湿度' },
-      ],
     },
     {
-      key: 'sub3',
+      key: 'monitor',
       label: '实时监控',
-      icon: <DotChartOutlined />,
+      icon: <CameraOutlined />,
     },
     {
-      key: 'sub4',
+      key: 'photoWall',
       label: '历时照片',
       icon: <FileImageOutlined />,
     },
     {
-      key: 'sub5',
+      key: 'hardwareControl',
       label: '硬件控制',
       icon: <WindowsOutlined />,
       children: [
-        { key: '3', label: '灯具' },
-        { key: '4', label: '音箱' },
-        { key: '5', label: '风扇' },
-        { key: '6', label: '数码管' },
+        { key: 'lamp', label: '灯具' },
+        { key: 'speakers', label: '音箱' },
+        { key: 'fan', label: '风扇' },
+        { key: 'digitalTube', label: '数码管' },
       ],
     },
     {
-      key: 'sub6',
+      key: 'me',
       label: '个人中心',
-      icon: <FileImageOutlined />,
+      icon: <UserOutlined />,
     },
   ];
 
@@ -54,8 +50,10 @@ function App() {
           万家灯火
         </div>
 
-        <div>
-          <span></span>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+          <span>
+            <Avatar size={16} icon={<UserOutlined />} />
+          </span>
           <span style={{ color: 'rgba(0, 0, 0, 0.45)', fontSize: '14px' }}>Raphael_Hu</span>
         </div>
       </div>
@@ -64,8 +62,8 @@ function App() {
         <div style={{ borderRight: '1px solid rgba(5, 5, 5, 0.06)', padding: '0px 8px' }}>
           <Menu
             style={{ width: 256, background: 'linear-gradient(#ffffff, #f5f5f5 28%)', border: 'none' }}
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub2']}
+            defaultSelectedKeys={['home']}
+            defaultOpenKeys={['hardwareControl']}
             mode="inline"
             items={items}
           />
