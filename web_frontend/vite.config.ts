ec782c92ad543e102cb8b69f64cc5a6740dbfd1e
Raphael <604332093@qq.com>
2025-05-30 14:14:16 +0800

    feat: 前端增加反向代理

--- web_frontend/vite.config.ts
+++ web_frontend/vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/cgi': {
+        target: 'http://192.168.1.200',
+        changeOrigin: true,
+      },
+    }
+  },
   plugins: [
     react(),
     tailwindcss()
