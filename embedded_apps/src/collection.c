41bd2d0f4430a15b1aec198febe314a9e4d40d78
Raphael <604332093@qq.com>
2025-06-05 16:14:37 +0800

    feat: update

--- embedded_apps/src/collection.c
+++ embedded_apps/src/collection.c
@@ -3,51 +3,44 @@
 #include <sys/shm.h>
 #include <stdio.h>
 #include <sys/msg.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <pthread.h>
 // #include "../../embedded_common/lib/cjson/cJSON.h"
 
-int collection_adc();
-int collection_accelerator();
-int collection_gyroscope();
-int collection_zeebig();
+int get_adc();
+int get_accelerator();
+int get_gyroscope();
+int get_zeebig();
 
 void* collection_thread() {
     long threadId = pthread_self();
-    printf("当前线程caijixiancheng, id: %lu\n", threadId);
     
-    int adc_data = collection_adc();
-    int accelerator_data = collection_accelerator();
-    int gyroscope_data = collection_gyroscope();
-    int zeebig_data = collection_zeebig();
+    int adc_data = get_adc();
+    int accelerator_data = get_accelerator();
+    int gyroscope_data = get_gyroscope();
+    int zeebig_data = get_zeebig();
 
     key_t key = ftok("/tmp/env.txt", 65);
     int shmid = shmget(key, 512, IPC_CREAT|0666);
     char* content = shmat(shmid, NULL, 0);
+    // bzero(content,512);
     strcpy(content, "a");
-    printf("%s\n", content);
-
-    // key_t key = ftok("/tmp/env.txt", 65);
-    // int msg_id = msgget(key, IPC_CREAT|0666);
-    // printf("key: %d\n", key);
-    // printf("msg_id: %d\n", msg_id);
-    // struct message { long type; char text[5] }msg = { 3, "a" };
-    // int result = msgsnd(msg_id, &msg, strlen(msg.text)+1, 0);
-    // printf("%s\n", msg.text);
-    
-    printf("sead end\n");
+    printf("shmat: %s\n", content);
 }
 
-int collection_adc() {
+int get_adc() {
     return 0;
 }
 
-int collection_accelerator() {
+int get_accelerator() {
     return 0;
 }
 
-int collection_gyroscope() {
+int get_gyroscope() {
     return 0;
 }
 
-int collection_zeebig() {
+int get_zeebig() {
     return 0;
 }
--- embedded_backend/src/environment.c
+++ embedded_backend/src/environment.c
@@ -3,15 +3,40 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <stdbool.h>
-#include <sys/msg.h>
+#include <sys/shm.h>
+#include <string.h>
 #include "../../embedded_common/lib/cjson/cJSON.h"
 #include "../include/format_response.h"
 
-int main() {
-    const char* method = getenv("REQUEST_METHOD");   
+struct Item {
+    char* name;
+    float count;
+};
+
+cJSON* format_array(struct Item list[]) {
+    cJSON *array = cJSON_CreateArray();
     
-    // 设置HTTP响应头
+    int i;
+    int length = sizeof(list) / sizeof(struct Item);
+    if (array == NULL) {
+        return NULL;
+    }
+    
+    for (i = 0; i < length; ++i) {
+        cJSON *obj = cJSON_CreateObject();
+        if (obj == NULL) return NULL;
+        cJSON_AddStringToObject(obj, "name", list[i].name);
+        cJSON_AddNumberToObject(obj, "count", list[i].count);
+        cJSON_AddItemToArray(array, obj);
+    }
+
+    return array;
+}
+
+int main() {
     printf("Content-Type: application/json\r\n\r\n");
+
+    const char* method = getenv("REQUEST_METHOD");   
     if (method == NULL) {
         format_response(-1, NULL, false);
         return -1;
@@ -20,31 +45,35 @@ int main() {
         format_response(-1, NULL, false);
         return -1;
     }
-
-    // key_t key = ftok("/tmp/env.txt", 65);
-    // int msg_id = msgget(key, IPC_CREAT|0666);
-    // if (msg_id == -1) {
-    //     format_response(-1, NULL, false);
-    //     return -1;
-    // }
-    // struct Message { long type; char text[5] }msg;
-    // ssize_t result = msgrcv(msg_id, &msg, sizeof(msg) - sizeof(long), 3, IPC_NOWAIT);
-    // if (!result) {
-    //     format_response(-1, NULL, false);
-    //     return -1;
-    // }
-    // if (!msg.type) {
-    //     format_response(0, NULL, true);   
-    //     return -1; 
-    // }
-    // cJSON* json_string  = cJSON_CreateString(msg.text);
-    // format_response(2, json_string, true);
     
     key_t key = ftok("/tmp/env.txt", 65);
     int shmid = shmget(key, 512, 0666);
-    char* buf = shmat(shmid, NULL, 0);
-    cJSON* json_string  = cJSON_CreateString(buf);
-    format_response(2, json_string, true);
-
+    char* content = shmat(shmid, NULL, 0);
+    // bzero(content,512);
+    // cJSON* json_string  = cJSON_CreateString(content);
+    
+    struct Item a9_list[9] = {
+        { 'Adc', 9.00 },
+        { 'CYROX', -14 },
+        { 'CYROY', 20 },
+        { 'CYROZ', 40 },
+        { 'AACX', 642 },
+        { 'AACY', -34 },
+        { 'AACZ', 5002 },
+        { 'A9-RESERVED-0', 0 },
+        { 'A9-RESERVED-1', 0 },
+    };
+    struct Item zeebig_list[2] = {
+        { name: 'Temperature', count: 10.00 },
+        { name: 'Humidity', count: 20.00 },
+    };
+    cJSON* a9 = format_array(a9_list);
+    cJSON* zeebig = format_array(zeebig_list);
+    
+    cJSON *data = cJSON_CreateObject();
+    cJSON_AddItemToObject(data, "a9", a9);
+    cJSON_AddItemToObject(data, "zeebig", zeebig);
+    
+    format_response(0, data, true);
     return 0;
 }
