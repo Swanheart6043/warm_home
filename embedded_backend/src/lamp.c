4ffbd2d8199841e0c4175b74f7190d3ffd002d46
Raphael <604332093@qq.com>
2025-06-09 17:49:08 +0800

    feat: 优化代码

--- embedded_backend/src/lamp.c
+++ embedded_backend/src/lamp.c
@@ -8,11 +8,12 @@
 #include "../include/format_response.h"
 
 int main() {
-    const char* method = getenv("REQUEST_METHOD");
     // 设置HTTP响应头
     printf("Content-Type: application/json\r\n\r\n");
+
+    const char* method = getenv("REQUEST_METHOD");
     if (strcmp(method, "POST") != 0) {
-        format_response(-1, NULL, false);
+        format_response(-1, cJSON_CreateString("请求方式错误"), false);
         return -1;
     }
     
@@ -20,13 +21,13 @@ int main() {
     char* content_length_str = getenv("CONTENT_LENGTH");
     int content_length = content_length_str ? atoi(content_length_str) : 0;
     if (content_length <= 0) {
-        format_response(-1, NULL, false);
+        format_response(-1, cJSON_CreateString("请求参数不能为空"), false);
         return -1;
     }
     // 读取POST数据
     char* input = (char *)malloc(content_length + 1);
     if (input == NULL) {
-        format_response(-1, NULL, false);
+        format_response(-1, cJSON_CreateString("服务器异常"), false);
         return -1;
     }
     fread(input, content_length, 1, stdin);
@@ -36,33 +37,35 @@ int main() {
     cJSON* json = cJSON_Parse(input);
     free(input);
     if (json == NULL) {
-        format_response(-1, NULL, false);
+        format_response(-1, cJSON_CreateString("服务器异常"), false);
+        cJSON_Delete(json);
         return -1;
     }
     // 处理具体的命令
     cJSON *operate = cJSON_GetObjectItemCaseSensitive(json, "operate");
     if (!cJSON_IsString(operate)) {
-        format_response(-1, NULL, false);
+        format_response(-1, cJSON_CreateString("服务器异常"), false);
+        cJSON_Delete(json);
         return -1;
     }
 
-    // send msg
+    // 通知应用层
     key_t key = ftok("/tmp", 'g');
     int msgid = msgget(key, IPC_CREAT|0666);
     if (key == -1 || msgid == -1) {
-        format_response(-1, cJSON_CreateString("fu wu qi yi chang"), false);
+        format_response(-1, cJSON_CreateString("服务器异常"), false);
         cJSON_Delete(json);
         return -1;
     }
     struct message { long type; char text[5] } msg = { 2, "on 1" };
     int result = msgsnd(msgid, &msg, strlen(msg.text)+1, 0);
     if (result < 0) {
-        format_response(-1, cJSON_CreateString("fu wu qi yi chang"), false);
+        format_response(-1, cJSON_CreateString("服务器异常"), false);
         cJSON_Delete(json);
         return -1;
     }
     
-    format_response(0, cJSON_CreateString("fu wu qi yi chang"), true);
+    format_response(0, cJSON_CreateString("操作成功"), true);
     cJSON_Delete(json);
     return 0;
 }
