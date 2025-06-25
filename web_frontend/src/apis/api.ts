510a3e2084df0463b257bf74004d968981e6eb98
Raphael <604332093@qq.com>
2025-06-25 23:44:21 +0800

    feat: update

--- web_frontend/src/apis/api.ts
+++ web_frontend/src/apis/api.ts
@@ -2,31 +2,31 @@ import axios from "axios"
 import type { Response, ResponseList, Control, Environmenta, ControlRequest } from "./apiType"
 
 export const fetchControlData = async () => {
-  const result = await axios.get<Response<Control>>('/cgi-bin/control.cgi')
+  const result = await axios.get<Response<Control>>('/api/control')
   return result.data
 }
 
 export const updateLamp = async (params: ControlRequest) => {
-  const result = await axios.post<Response<null>>('/cgi-bin/lamp.cgi', params)
+  const result = await axios.post<Response<null>>('/api/lamp', params)
   return result.data
 }
 
 export const updateSpeakers = async (params: ControlRequest) => {
-  const result = await axios.post<Response<null>>('/cgi-bin/speakers.cgi', params)
+  const result = await axios.post<Response<null>>('/api/speakers', params)
   return result.data
 }
 
 export const updateFan = async (params: ControlRequest) => {
-  const result = await axios.post<Response<null>>('/cgi-bin/fan.cgi', params)
+  const result = await axios.post<Response<null>>('/api/fan', params)
   return result.data
 }
 
 export const fetchEnvironmentalData = async () => {
-  const result = await axios.get<Response<Environmenta>>('/cgi-bin/env.cgi')
+  const result = await axios.get<Response<Environmenta>>('/api/env')
   return result.data
 }
 
 export const fetchPhotoWall = async () => {
-  const result = await axios.get<ResponseList<string>>('/cgi-bin/photo.cgi')
+  const result = await axios.get<ResponseList<string>>('/api/photo')
   return result.data
 }
--- web_frontend/vite.config.ts
+++ web_frontend/vite.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/cgi': {
-        target: 'http://192.168.1.100',
+      '/api': {
+        target: 'http://192.168.1.200:8080',
         changeOrigin: true,
       },
     }
