73a4adaf908c4dc8bcfff0c5f050223647b2178e
Raphael <604332093@qq.com>
2025-06-26 00:13:38 +0800

    feat: update

--- web_frontend/vite.config.ts
+++ web_frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://192.168.1.200:8080',
+        target: 'http://192.168.1.100:8080',
         changeOrigin: true,
       },
     }
